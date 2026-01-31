3a718e4313df379a43d40cf9eee0b1f0437e0192
Tamerlan12345 <centr-cool@mail.ru>
2026-01-31 21:14:32 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -206,4 +206,4 @@ async def websocket_endpoint(websocket: WebSocket):
             pass
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=port, ws="wsproto")
